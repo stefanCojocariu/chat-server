4ea9e366d39c79d515f2edfbe61b6c5254d84ba9
umbra179 <46658934+umbra179@users.noreply.github.com>
2021-07-21 20:24:27 +0300

    validations

--- src/models/user.ts
+++ src/models/user.ts
@@ -12,18 +12,36 @@ export interface IUser extends moongose.Document {
     socketId: string
 };
 
+let validateEmail = function(email:string) {
+    var re = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/;
+    return re.test(email)
+};
+
+let validateUsername = function(username:string){
+    var re = /^[a-z0-9_\.]+$/;
+    return re.test(username);
+}
+
 const UserSchema = new Schema({
     name: {
         type: String,
     },
     username: {
         type: String,
+        trim: true,
+        lowercase: true,
+        required: 'Username is required',
+        validate: [validateUsername, 'Please fill a valid username (alphanumeric + "_" + ".")']
     },
     password: {
         type: String,
     },
     email: {
         type: String,
+        trim: true,
+        lowercase: true,
+        required: 'Email address is required',
+        validate: [validateEmail, 'Please fill a valid email address']
     },
     date: {
         type: String,
---
+++ src/utils/validators.ts
@@ -0,0 +1,15 @@
+class Validators {
+    constructor(){}
+
+    validateUsername(username:string){
+        var re = /^[a-z0-9_\.]+$/;
+        return re.test(username);
+    }
+
+    validateEmail(email:string) {
+        var re = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/;
+        return re.test(email)
+    };
+}
+
+export default Validators;
