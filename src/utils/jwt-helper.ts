9809bd6fdec469b501070bc9cbe021f712be3159
stefanCojocariu <stefancojocariu19@yahoo.ro>
2021-07-24 11:49:23 +0300

    -wip

--- src/utils/jwt-helper.ts
+++ src/utils/jwt-helper.ts
@@ -1,60 +1,58 @@
 import { IUser } from "../models/user";
-import JWT from 'jsonwebtoken'
+import JWT, { JwtPayload } from 'jsonwebtoken';
+import error from '../configs/error.constants';
 
 export interface Tokens {
-    accessToken:string,
-    refreshToken:string
+    accessToken: string,
+    refreshToken: string
 }
 
 class JWTHelper {
     constructor() { }
-    signAccessToken(userObj: IUser): Promise<string> {
-        return new Promise(
-            async (resolve, reject) => {
-                try{
-                    const payload = userObj;
-                    const secret = process.env.ACCESS_TOKEN_SECRET;
-                    if (!secret) {
-                        reject('no jwt secret');
-                        return;
-                    }
-                    const signOptions = {
-                        expiresIn: process.env.ACCESS_TOKEN_EXPIRESIN,
-                        audience: userObj.username
-                    };
-    
-                    const accessToken = await JWT.sign(payload, secret, signOptions);
-                    resolve(accessToken);
-                } catch (error) {
-                    reject(error);
-                }
-            }
-        )
+
+    async signAccessToken(userObj: IUser): Promise<string> {
+        const payload = userObj;
+        const secret = process.env.ACCESS_TOKEN_SECRET;
+        if (!secret) {
+            throw error.JWT_NOSECRET;
+        }
+        const signOptions = {
+            expiresIn: process.env.ACCESS_TOKEN_EXPIRESIN,
+            audience: userObj.username
+        };
+
+        return await JWT.sign(payload, secret, signOptions);
     }
 
-    signRefreshToken(userObj: IUser): Promise<string> {
-        return new Promise(
-            async (resolve, reject) => {
-                try{
-                    const payload = userObj;
-                    const secret = process.env.REFRESH_TOKEN_SECRET;
-                    if (!secret) {
-                        reject('no jwt secret');
-                        return;
-                    }
-                    //!!!!! WILL BE ALWAYS THE SAME - EPIRES IN SHOW TAKE CURRENT DATE + env variable
-                    const signOptions = {
-                        expiresIn: process.env.REFRESH_TOKEN_EXPIRESIN,
-                        audience: userObj.username
-                    };
-    
-                    const refreshToken = await JWT.sign(payload, secret, signOptions);
-                    resolve(refreshToken);
-                } catch (error) {
-                    reject(error);
-                }
-            }
-        )
+    async signRefreshToken(userObj: IUser): Promise<string> {
+        const payload = userObj;
+        const secret = process.env.REFRESH_TOKEN_SECRET;
+        if (!secret) {
+            throw error.JWT_NOSECRET;
+        }
+        //!!!!! WILL BE ALWAYS THE SAME - EPIRES IN SHOW TAKE CURRENT DATE + env variable
+        const signOptions = {
+            expiresIn: process.env.REFRESH_TOKEN_EXPIRESIN,
+            audience: userObj.username
+        };
+
+        return await JWT.sign(payload, secret, signOptions);
+    }
+
+    async verifyAccessToken(accessToken: string): Promise<string | JwtPayload> {
+        const secret = process.env.REFRESH_TOKEN_SECRET;
+        if (!secret) {
+            throw error.JWT_NOSECRET;
+        }
+        return await JWT.verify(accessToken, secret);
+    }
+
+    async verifyRefreshToken(refreshToken: string): Promise<string | JwtPayload> {
+        const secret = process.env.REFRESH_TOKEN_SECRET;
+        if (!secret) {
+            throw error.JWT_NOSECRET;
+        }
+        return await JWT.verify(refreshToken, secret)
     }
 }
 
